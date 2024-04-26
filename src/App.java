10e582c814647e7b3128f260435cacf276d33992
PhillipeDiniz19 <phillipediniz19@gmail.com>
2024-04-26 14:38:11 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception { 
+        System.out.println("Hello, World!");
+    }
+}
+
+
+// "Public" torna a classe pública para acessar 
+// "Static" 
+// "Void" vai executar a fução "main" e não vai retornar nada.
+// "System" tem saidas de input e output para ser usado na aplicação.
