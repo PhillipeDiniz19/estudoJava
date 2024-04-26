f94f062024322db7a8fd720e2cfe02c99db3cac3
PhillipeDiniz19 <phillipediniz19@gmail.com>
2024-04-26 14:53:46 -0300

    tipos de var

---
+++ src/Tipos.java
@@ -0,0 +1,10 @@
+public class Tipos {
+  public static void main(String [] args) {
+    int ano = 2021; // passa uma var chamada ano de tipo inteiro
+    ano = 2020; // altero o ano da var
+
+    final String BR = "Brasil"; //var toda maiúscula ele pode ser considerado como uma "const" valor n muda, tem que passar "final" 
+    // BR = "brazil";
+    System.out.println(BR);
+  }
+}
