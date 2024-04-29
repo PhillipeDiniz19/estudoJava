6e7c36997e91825dfa34394d35b93af5202149e5
PhillipeDiniz19 <phillipediniz19@gmail.com>
2024-04-29 15:56:24 -0300

    atulizando java2

---
+++ src/Boletim.java
@@ -0,0 +1,12 @@
+public class Boletim {
+  public static void main(String[] args) {
+    int mediaFinal = 4;
+    if(mediaFinal > 6){
+      System.out.println("Parabens você foi aprovado.");
+    }else if(mediaFinal < 6) {
+      System.out.println("Você foi reprovado.");
+    }else {
+      System.out.println("Você ficou na média");
+    }
+  }
+}
