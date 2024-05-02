ca49fce75276edd65e7d842af083bb481068d09d
PhillipeDiniz19 <phillipediniz19@gmail.com>
2024-05-02 14:05:39 -0300

    tipos prmitivos

--- src/edu.ph.segundaSemana/tiposVari.java
+++ src/edu.ph.segundaSemana/tiposVari.java
@@ -1,7 +1,14 @@
 
 public class tiposVari {
     public static void main(String[] args) {
-    String [] nomes = {"phillipe", "diniz", "cardoso"};
-    System.out.println(nomes);
+      int idade;
+      int anoDeFabricacao = 2024;
+      double salarioMinino = 2.500;
+      byte idadeFinal = 20; // valores entre -128 ate 127
+      short ano = 2021;
+      int cep = 72231116;
+      long cpf = 1234567890;
+      float pi = 3.14F; // Tipo float tem que terminar com F se nao ele acha que é um tipo double
+      
   }
 }
