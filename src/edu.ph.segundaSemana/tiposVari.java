d708448d8f6f1748018264ec614e6a9c096fbcaf
PhillipeDiniz19 <phillipediniz19@gmail.com>
2024-05-02 14:24:08 -0300

    tipos const

--- src/edu.ph.segundaSemana/tiposVari.java
+++ src/edu.ph.segundaSemana/tiposVari.java
@@ -9,6 +9,11 @@ public class tiposVari {
       int cep = 72231116;
       long cpf = 1234567890;
       float pi = 3.14F; // Tipo float tem que terminar com F se nao ele acha que é um tipo double
+
+      int numero = 10;
+
+      final double VALOR_DE_PI = 3.14; // "final" sempre vai transformar a variavel em "const"
+      System.out.println(VALOR_DE_PI); // Quando for uma constante sempre colocar em caixa alta para melhor representação
       
   }
 }
