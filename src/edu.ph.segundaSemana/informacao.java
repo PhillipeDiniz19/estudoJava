ea879d16f48e65de034228ab52090fc7a287b352
PhillipeDiniz19 <phillipediniz19@gmail.com>
2024-05-03 15:12:42 -0300

    att2

--- src/edu.ph.segundaSemana/informacao.java
+++ src/edu.ph.segundaSemana/informacao.java
@@ -3,12 +3,16 @@ import java.util.Scanner;
 public class informacao {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-          System.out.println("Digite o segundo primeiro: ");
-          final int informacao = scanner.nextLine();
-          System.out.println("Digite o segundo numero: ");
-          final int informacaoDois = scanner.nextLine();
-          int resultado = informacao + informacaoDois;
-          System.out.println("A soma dos dois numeros é: " + informacao );
+        
+        System.out.println("Digite o primeiro número: ");
+        int informacao = Integer.parseInt(scanner.nextLine());
+        
+        System.out.println("Digite o segundo número: ");
+        int informacaoDois = Integer.parseInt(scanner.nextLine());
+        
+        int resultado = informacao + informacaoDois;
+        
+        System.out.println("A soma dos dois números é: " + resultado);
 
         scanner.close();
     }
