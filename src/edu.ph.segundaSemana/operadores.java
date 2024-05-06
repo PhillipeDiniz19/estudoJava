e818c8468590b6c5b16403d90b0735223d6a89b1
PhillipeDiniz19 <phillipediniz19@gmail.com>
2024-05-06 11:10:36 -0300

    trueOrFalse

--- src/edu.ph.segundaSemana/operadores.java
+++ src/edu.ph.segundaSemana/operadores.java
@@ -1,31 +1,42 @@
 public class operadores {
   public static void main(String[] args) {
-    double soma = 10.5 + 15.7;
-    int subtração = 113 - 25;
-    int multiplicacao = 20 * 7;
-    int divisao = 15 / 3;
-    int modulo = 18 % 3;
-    double resultado = (10 * 7) + (20/4);
+    // double soma = 10.5 + 15.7;
+    // int subtração = 113 - 25;
+    // int multiplicacao = 20 * 7;
+    // int divisao = 15 / 3;
+    // int modulo = 18 % 3;
+    // double resultado = (10 * 7) + (20/4);
 
-    String nomeCompleto = "LINGUAGEM" + "jAVA";
-    System.out.println(nomeCompleto);
+    // String nomeCompleto = "LINGUAGEM" + "jAVA";
+    // System.out.println(nomeCompleto);
 
-    String concatenacao = "?";
-    System.out.println(concatenacao);
-    concatenacao = 1+1+1+"1";
-    System.out.println(concatenacao);
-    concatenacao = 1+"1"+1+1;
-    System.out.println(concatenacao);
-    concatenacao = 1+"1"+1+"1";
-    System.out.println(concatenacao);
-    concatenacao = "1"+1+1+1;
-    System.out.println(concatenacao);
+    // String concatenacao = "?";
+    // System.out.println(concatenacao);
+    // concatenacao = 1+1+1+"1";
+    // System.out.println(concatenacao);
+    // concatenacao = 1+"1"+1+1;
+    // System.out.println(concatenacao);
+    // concatenacao = 1+"1"+1+"1";
+    // System.out.println(concatenacao);
+    // concatenacao = "1"+1+1+1;
+    // System.out.println(concatenacao);
+
+    // int numero = 5;
+    // numero = - numero;
+    // System.out.println(numero);
+    // numero = + numero;
+    // System.out.println(numero);
+
+    //Incrementar numero
 
     int numero = 5;
-    numero = - numero;
+    // -> mesma coisa que numero = numero + 1
+    System.out.println(++ numero); // adiciona como prefixo soma primeiro e imprime depois
+    System.out.println(numero ++ );// imprime primiro e depois coloca a soma na var
     System.out.println(numero);
-    numero = + numero;
-    System.out.println(numero);
+
+    boolean trueOrFalse = true;
+    System.out.println(!trueOrFalse); // ! vai inverte o valor da variavel
 
   }
 }
