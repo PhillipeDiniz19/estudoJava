18db3d8345f03405fb4fe7e03ef5d715b893b063
PhillipeDiniz19 <phillipediniz19@gmail.com>
2024-05-06 11:18:52 -0300

    operador ternario

---
+++ src/edu.ph.segundaSemana/ternario.java
@@ -0,0 +1,18 @@
+public class ternario {
+  public static void main(String[] args) {
+    int a, b;
+
+    a = 6;
+    b = 7;
+
+    String resultado = a==b ? "verdadeiro" : "false";
+
+    // if(a==b) 
+    //   resultado = "verdadeiro";
+    // else  
+    //   resultado = "false";  
+
+    System.out.println(resultado);  
+
+  }
+}
