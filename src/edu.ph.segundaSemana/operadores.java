437e0c25b3ae16f7629a0fa08303b7be1ed0e27d
PhillipeDiniz19 <phillipediniz19@gmail.com>
2024-05-06 10:59:03 -0300

    operadores ari

--- src/edu.ph.segundaSemana/operadores.java
+++ src/edu.ph.segundaSemana/operadores.java
@@ -21,6 +21,11 @@ public class operadores {
     concatenacao = "1"+1+1+1;
     System.out.println(concatenacao);
 
+    int numero = 5;
+    numero = - numero;
+    System.out.println(numero);
+    numero = + numero;
+    System.out.println(numero);
 
   }
 }
