1867bcfdddaaa80d6d7d515a9b7490143ea716bb
PhillipeDiniz19 <phillipediniz19@gmail.com>
2024-05-08 11:37:08 -0300

    operadores logicos

---
+++ src/edu.ph.terceiraSemana/opera.java
@@ -0,0 +1,18 @@
+public class opera {
+  public static void main(String[] args) {
+    int numero1 = 1;
+    int numero2 = 2;
+
+    boolean simNao = numero1 == numero2;
+
+    if(numero1 == numero2) {
+      System.out.println("A nossa condição é verdadeira");
+    }else {
+    System.out.println("numeroUm é igual ao numeroDois? " + simNao);
+      simNao = numero1 != numero2;
+    System.out.println("numeroUm é diferente ao numeroDois? " + simNao);  
+      simNao = numero1 > numero2;
+    System.out.println("numeroUm é maior que numeroDois? " + simNao);
+    }
+  }
+}
