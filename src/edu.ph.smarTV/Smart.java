f6d75d88607c857a20e71d03fbd82f5c56b54b12
PhillipeDiniz19 <phillipediniz19@gmail.com>
2024-05-09 20:08:54 -0300

    alternando valor do volume com metados

--- src/edu.ph.smarTV/Smart.java
+++ src/edu.ph.smarTV/Smart.java
@@ -1,6 +1,21 @@
 public class Smart {
-  boolean ligada = false;
+  boolean ligado = false;
   int canal = 1;
   int volume = 25;
   
+  public void Ligar() {
+    ligado = true;
+  }
+  public void Desligar() {
+    ligado = false;
+  }
+  public void AumentarVolume() {
+    volume++;
+    System.out.println("Aumentando volume: " + volume);
+  }
+  public void DiminuirVolume() {
+    volume--;
+    System.out.println("Diminuindo volume: " + volume);
+  }
+
 }
--- src/edu.ph.smarTV/Usuario.java
+++ src/edu.ph.smarTV/Usuario.java
@@ -2,8 +2,22 @@ public class Usuario {
   public static void main(String[] args) throws Exception {
     Smart smart = new Smart();
 
-    System.out.println("Tv esta ligada? " + smart.ligada);
+    System.out.println("Tv esta ligada? " + smart.ligado);
     System.out.println("Canal atual = " + smart.canal);
     System.out.println("Volume atual: " + smart.volume);
+
+    smart.Ligar();
+
+    System.out.println("Tv esta ligada? " + smart.ligado);
+
+    smart.Desligar();
+
+    System.out.println("Tv esta ligada? " + smart.ligado);
+
+    smart.AumentarVolume();
+    smart.AumentarVolume();
+    smart.AumentarVolume();
+    smart.DiminuirVolume();
+    
   }
 }
