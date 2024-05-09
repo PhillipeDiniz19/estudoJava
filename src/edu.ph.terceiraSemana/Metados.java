eb0ec2aae2bc3ac7fae63de07aa9758a32d479ae
PhillipeDiniz19 <phillipediniz19@gmail.com>
2024-05-09 19:55:02 -0300

    metados em java

---
+++ src/edu.ph.terceiraSemana/Metados.java
@@ -0,0 +1,12 @@
+public class Metados {
+  
+  // Método para somar dois números
+  public static double somar(int num1, int num2) {
+    return num1 + num2;
+  }
+  
+  public static void main(String[] args) {
+    // Chamando o método somar e imprimindo o resultado
+    System.out.println("Resultado da soma: " + somar(5, 3));
+  }
+}
