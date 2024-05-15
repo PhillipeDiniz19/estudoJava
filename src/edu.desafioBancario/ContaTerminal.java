ff16900a1c50b92cf5ff100d291e8652f89e64e9
PhillipeDiniz19 <phillipediniz19@gmail.com>
2024-05-15 16:22:38 -0300

    projeto dio java

--- src/edu.desafioBancario/ContaTerminal.java
+++ src/edu.desafioBancario/ContaTerminal.java
@@ -1,3 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-  
+  public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+    System.out.println("Digite o numero da conta: ");
+      int NumeroConta = scanner.nextInt();
+
+    System.out.println("Digite o numero da agencia: ");
+      String NumeroAgencia = scanner.next();
+
+    System.out.println("Digite o nome do cliente: ");
+      String NomeCliente = scanner.next();
+    
+    System.out.println("Digite o saldo do cliente: "); 
+      double SaldoCliente = scanner.nextInt();
+      
+    System.out.println("Numero da conta é: " + NumeroConta);
+    System.out.println("Numero da agencia é: " + NumeroAgencia);    
+    System.out.println("Nome do cliente é: " + NomeCliente);  
+    System.out.println("Saldo do cliente é: " + SaldoCliente);  
+  }
 }
