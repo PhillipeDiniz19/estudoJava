1b9b582fa9c16790411b42876a9fd841f0615471
PhillipeDiniz19 <phillipediniz19@gmail.com>
2024-05-16 18:26:24 -0300

    operadores ternarios.

--- src/edu.ph.quartaSemana/Estruturas.java
+++ src/edu.ph.quartaSemana/Estruturas.java
@@ -9,17 +9,26 @@ public class Estruturas {
     System.out.println("Saldo é : " + saldo);
 
     IfElse(args); // Chama o método IfElse
+    Ternario(args); // Chamar 3 public
   }
 
   public static void IfElse(String[] args) {
-    int nota = 7;
+    int nota = 4;
 
     if(nota >= 7){
       System.out.println("Aluno aprovado");
+    }else if(nota < 7 && nota >= 5){
+      System.out.println("Aluno de recuperação");
     }else{
       System.out.println("Aluno reprovado!!!");
     }
   }
 
-  
+  public static void Ternario(String[] args) {
+      int notaUm = 4;
+      String resultado = notaUm >= 7 ? "aprovado" : "Reprovado";
+      String resultadoDois = notaUm >= 7 ? "aprovado" : notaUm >= 5 && notaUm < 7 ? "Recuperação" : "Reprovado" ;
+      System.out.println(resultado);
+      System.out.println(resultadoDois);
+  }
 }
