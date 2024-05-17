51e5226f3c41088f20d2046162e334f4493549df
PhillipeDiniz19 <phillipediniz19@gmail.com>
2024-05-17 16:28:13 -0300

    for em arrays

---
+++ src/edu.ph.quartaSemana/Repeticao.java
@@ -0,0 +1,7 @@
+public class Repeticao{
+  public static void main(String[] args){
+    for(int caneiros = 1; caneiros <= 20; caneiros++){
+      System.out.println("Contando os caneiros: "+ caneiros);
+    }
+  }
+}
