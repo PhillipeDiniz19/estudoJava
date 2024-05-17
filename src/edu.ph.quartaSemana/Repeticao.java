520b04296f34a8a5d1f03adcf82bd583eb772743
PhillipeDiniz19 <phillipediniz19@gmail.com>
2024-05-17 16:47:25 -0300

    valor da mesada usando while

--- src/edu.ph.quartaSemana/Repeticao.java
+++ src/edu.ph.quartaSemana/Repeticao.java
@@ -1,3 +1,5 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Repeticao{
   public static void main(String[] args){
     for(int caneiros = 1; caneiros <= 20; caneiros++){
@@ -14,5 +16,28 @@ public class Repeticao{
         break;
       }
     }
+    RepeticaoWhile(args);
+  }
+
+  public static void RepeticaoWhile(String[] args) {
+    double mesada = 50.0;
+        while(mesada>0) {
+            Double valorDoce = valorAleatorio();
+            if(valorDoce > mesada)
+                valorDoce = mesada;
+
+            System.out.println("Doce do valor: " + valorDoce + " Adicionado no carrinho");
+            mesada = mesada - valorDoce;
+        }
+        System.out.println("Mesada:" + mesada);
+        System.out.println("Joãozinho gastou toda a sua mesada em doces");
+        
+        /*
+        * Não se preocupe quanto a formatação de valores
+        * Iremos explorar os recursos de formatação em breve !!
+        */
+   }
+   private static double valorAleatorio() {
+	return ThreadLocalRandom.current().nextDouble(2, 8);
   }
 }
