03e6659f402e7a73dc93eb88c7fc18b43a43d03b
PhillipeDiniz19 <phillipediniz19@gmail.com>
2024-05-17 16:49:16 -0300

    DoWhile

--- src/edu.ph.quartaSemana/Repeticao.java
+++ src/edu.ph.quartaSemana/Repeticao.java
@@ -1,4 +1,6 @@
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
+
 
 public class Repeticao{
   public static void main(String[] args){
@@ -17,6 +19,7 @@ public class Repeticao{
       }
     }
     RepeticaoWhile(args);
+    usaDoWhile(args);
   }
 
   public static void RepeticaoWhile(String[] args) {
@@ -40,4 +43,22 @@ public class Repeticao{
    private static double valorAleatorio() {
 	return ThreadLocalRandom.current().nextDouble(2, 8);
   }
+
+  public static void usaDoWhile(String[] args) {
+    System.out.println("Discando...");
+		
+		do {
+			//excutando repetidas vezes até alguém atender
+			System.out.println("Telefone tocando");
+		
+		}while(tocando());
+		
+		System.out.println("Alô !!!");
+	}
+	private static boolean tocando() {
+		boolean atendeu = new Random().nextInt(3)==1;
+		System.out.println("Atendeu? " + atendeu);
+		//negando o ato de continuar tocando
+		return ! atendeu;
+	}
 }
