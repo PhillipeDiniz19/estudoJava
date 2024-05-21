5e0da45f9149a44765dfafca5fa7f41c5d074655
PhillipeDiniz19 <phillipediniz19@gmail.com>
2024-05-21 11:13:18 -0300

    Usando try / catch

--- src/edu.ph.quintasemana/TryCatch.java
+++ src/edu.ph.quintasemana/TryCatch.java
@@ -1,9 +1,13 @@
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
+// "TRY" Bloco de codigo a ser testado
+// "CATCH" Define um bloco de codigo caso o "TRY" apresente error
+
 public class TryCatch {
   public static void main(String[] args) {
-    //criando o objeto scanner
+   try {
     Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
     
     System.out.println("Digite seu nome");
@@ -24,5 +28,9 @@ public class TryCatch {
     System.out.println("Tenho " + idade + " anos ");
     System.out.println("Minha altura é " + altura + "cm ");
     scanner.close();   
+   } catch (InputMismatchException e) {
+    System.out.println("O campos idade e altura precisam ser númericos.");
+   }
+   
   }
 }
